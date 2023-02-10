f5d19b8b774853f3fa78a16cd01a1f41c05401c1
Darren Smith <darren@spacedog.com.au>
2023-02-10 18:32:43 +1100

    Add moderation tool functionality

--- canvas-custom-js/moderation-tool.js
+++ canvas-custom-js/moderation-tool.js
@@ -1,10 +1,13 @@
-/**
- // @name        Moderation tool
- // @namespace   https://github.com/dazweeja/subhive.github.io
- // @author      Darren Smith <darren@spacedog.com.au>
- // @updated     10.01.2023
- //
- **/
+// ==UserScript==
+// @name        Moderation tool
+// @namespace   https://github.com/dazweeja/subhive.github.io
+// @author      Darren Smith <darren@spacedog.com.au>
+// @description Script for adding active enrollments to canvas user page
+// @version     0.1
+// @match       https://collarts.instructure.com/*
+// @match       https://collarts.test.instructure.com/*
+// @run-at      document-end
+// ==/UserScript==
 (function () {
   'use strict';
 
@@ -13,6 +16,8 @@
 
   function init() {
     console.log('init');
+    if (!window.location.pathname.match(/\/courses\/\d+\/gradebook/)) return;
+
     const totalHeading = document.querySelector(selector);
     const config = {childList: true};
 
@@ -34,124 +39,80 @@
     }
   }
 
-  function addMenuItem(menu) {
-    if (getIsTeacher()) return;
-
-    const courseId = getCourseId();
-    const href = '/courses/' + courseId + '/pages/my-grades';
-    const menuItem = menu.querySelector('li a.modules');
-
-    if (menuItem) {
-      const isActive = window.location.href.indexOf(href) !== -1;
-      const item = document.createElement('li');
-      item.classList.add('section');
-      const link = document.createElement('a');
-      link.classList.add('my-grades');
-      if (isActive) {
-        link.classList.add('active');
-      }
-      link.setAttribute('href', href);
-      link.setAttribute('tabindex', '0');
-      link.innerText = 'My Grades';
-      item.append(link);
-      menuItem.parentNode.after(item);
-
-      if(isActive) {
-        const pagesItem = menu.querySelector('li a.pages');
-        pagesItem.classList.remove('active');
-      }
-    }
-  }
-
   function addModerationTotals(totalHeading) {
-    console.log('amt');
     if (!getIsTeacher()) return;
 
-    console.log('amt1');
-    console.log(totalHeading);
-
-    return;
-
-
     const courseId = getCourseId();
-    const url = baseUrl + '/api/v1/courses/' + courseId + '/assignment_groups?per_page=999&include[]=assignments';
+    const url = baseUrl + '/api/v1/courses/' + courseId + '/users?per_page=999&&enrollment_type[]=student&include[]=enrollments';
 
-    getAssignmentGroups(url)
-      .then(function (assignmentGroups) {
-        const grades = {};
-        const assignments = {};
-        const groups = [];
+    const icon = document.createElement('i');
+    icon.style.cursor = 'pointer';
+    icon.style.font = '0.75rem InstructureIcons-Solid';
+    icon.innerHTML = '&#xEB58;';
+    icon.style.marginLeft = '0.25rem';
+    totalHeading.append(icon);
 
-        assignmentGroups.sort((a, b) => a.position - b.position);
+    const wrapper = document.createElement('div');
+    wrapper.style.border = '1px solid #bbb';
+    wrapper.style.zIndex = '999';
+    wrapper.style.padding = '0 0 0.5rem';
+    wrapper.style.backgroundColor = '#fff';
+    wrapper.style.position = 'absolute';
+    wrapper.fontSize = '1rem';
 
-        assignmentGroups.forEach(function (assignmentGroup) {
-          console.log(assignmentGroup);
-          let group = null;
-          if (assignmentGroup.assignments && assignmentGroup.assignments.length) {
-            const groupAssignments = [];
-            assignmentGroup.assignments.sort((a, b) => a.position - b.position);
+    const bodyRect = document.body.getBoundingClientRect();
+    const headingRect = totalHeading.closest('div.slick-header-column').getBoundingClientRect();
+    wrapper.style.right = (bodyRect.right - headingRect.right) + 'px';
+    wrapper.style.top = (headingRect.bottom - bodyRect.top - 1) + 'px';
 
-            assignmentGroup.assignments.forEach(function (groupAssignment) {
-              const assignment = {
-                id: groupAssignment.id,
-                url: groupAssignment.html_url,
-                name: groupAssignment.name
-              };
+    const table = document.createElement('table');
+    table.style.width = '16rem';
+    table.style.border = '0';
+    table.style.fontSize = '0.875rem';
 
-              if (groupAssignment.grading_type === 'points') {
-                assignment.points_possible = 'points_possible' in groupAssignment ? groupAssignment.points_possible : -1;
-              }
+    const tableBody = document.createElement('tbody');
+    const loadingBody = createLoadingTableBody();
 
-              groupAssignments.push(assignment);
-              assignments[assignment.id] = assignment;
-              grades[assignment.id] = null;
-            });
+    icon.addEventListener('click', function(event) {
+      if (!document.body.append(wrapper)) {
+        table.append(tableBody);
+        wrapper.append(table);
+        document.body.append(wrapper);
+      }
 
-            if (group) {
-              group.assignments = group.assignments.concat(groupAssignments);
+      tableBody.innerHTML = loadingBody;
+
+        getUsers(url)
+        .then(function (users) {
+          users.forEach(function (user) {
+            if (user.name.match(/Test\s*Student/i)) return;
+
+            if (user.enrollments && user.enrollments.length > 0) {
+              let total = 0, fails = 0, passes = 0;
+              user.enrollments.forEach(function (enrollment) {
+                if (enrollment.enrollment_state !== 'active' || enrollment.type !== 'StudentEnrollment') return;
+
+                total++;
+
+                if (enrollment.current_score === null) {
+                  // ignore
+                }
+                else if (enrollment.current_score < 49.5) {
+                  fails++;
+                }
+                else {
+                  passes++;
+                }
+
+                tableBody.innerHTML = createResultsTableBody(total, fails, passes);
+              });
             }
-            else {
-              group = {name: assignmentGroup.name, assignments: groupAssignments};
-            }
-          }
-
-          if (group) {
-            groups.push(group);
-          }
+          });
         })
+        .catch(e => errorHandler(e))
 
-        const subUrl = new URL(baseUrl + '/api/v1/courses/' + courseId + '/students/submissions');
-        subUrl.searchParams.append('per_page', 999);
-        Object.keys(grades).forEach(val => subUrl.searchParams.append('assignment_ids[]', val));
-
-        getStatuses(subUrl)
-          .then(statuses => {
-            console.log(statuses);
-            console.log(grades);
-            parseStatuses(statuses, grades, assignments);
-
-            let content = '';
-            groups.forEach(function (group) {
-              content += `<h2>${group.name}</h2>`;
-              content += createTable(group, grades);
-            });
-
-            /*let isProgress = false;
-            let isIncompetent = false;
-            for (const assignmentId in grades) {
-              const grade = grades[assignmentId];
-              if (grade === 'submitted' || grade === 'not submitted' || grade === 'NA') isProgress = true;
-              else if (grade !== 'satisfactory' && grade !== 'excused') isIncompetent = true;
-            }
-
-            const result = isProgress ? 'In Progress' : (isIncompetent ? 'Not Yet Competent' : 'Competent');
-            content += createResultTable(result);*/
-
-            viewer.innerHTML = content;
-          })
-          .catch(e => errorHandler(e));
-      })
-      .catch(e => errorHandler(e));
+      console.log('end');
+    });
   }
 
   function getCourseId() {
@@ -187,11 +148,11 @@
     return isTeacher;
   }
 
-  function getAssignmentGroups(url) {
-    return getAssignmentGroupsPage(url)
+  function getUsers(url) {
+    return getUsersPage(url)
       .then(function (page) {
         if (page.url) {
-          return getAssignmentGroups(page.url)
+          return getUsersPage(page.url)
             .then(function (nextPage) {
               return page.data.concat(nextPage);
             });
@@ -203,7 +164,7 @@
       .catch(e => errorHandler(e));
   }
 
-  function getAssignmentGroupsPage(url) {
+  function getUsersPage(url) {
     return new Promise(function (resolve, reject) {
       const xhr = new XMLHttpRequest();
       xhr.open('GET', url, true);
@@ -227,120 +188,20 @@
     });
   }
 
-  function getStatuses(url) {
-    return getStatusPage(url)
-      .then(page => {
-        if (page.url) {
-          return getStatuses(page.url)
-            .then(nextPage => page.data.concat(nextPage));
-        }
-        else {
-          return page.data;
-        }
-      })
-      .catch(e => errorHandler(e));
+  function createResultsTableBody(total, fails, passes) {
+    let content = `<tr><td colspan="2" style="text-align: right"><i class="icon-Solid icon-refresh" aria-hidden="true"></i>&nbsp;<i class="icon-Solid icon-end" aria-hidden="true"></i></td></tr>`;
+    content += `<tr><td style="width: 85%; padding: 0.5rem 0 0 0.5rem;">Total students:</td><td style="width: 15%; text-align: center; padding: 0;">${total}</td></tr>`;
+    content += `<tr><td style="width: 85%; padding: 0.5rem 0 0 0.5rem;">Projected fails:</td><td style="width: 15%; text-align: center; padding: 0;">${fails}</td></tr>`;
+    content += `<tr><td style="width: 85%; padding: 0.5rem 0 0 0.5rem;">Projected passes (49.5% +):</td><td style="width: 15%; text-align: center; padding: 0;">${passes}</td></tr>`;
+
+    return content;
   }
 
-  function getStatusPage(url) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', url, true);
-      xhr.onload = () => {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            resolve({
-              data: JSON.parse(xhr.responseText),
-              url: nextURL(xhr.getResponseHeader('Link'))
-            })
-          }
-        }
-        else {
-          reject(xhr.statusText);
-        }
-      };
-      xhr.onerror = () => {
-        reject(xhr.statusText);
-      };
-      xhr.send();
-    });
-  }
-
-  function parseStatuses(statuses, grades, assignments) {
-    statuses.forEach(status => {
-      // workflow state: “submitted”, “unsubmitted”, “graded”, “pending_review”
-      let grade = status.excused ? 'excused' : status.grade;
-      if (status.workflow_state === 'submitted') {
-        grade = 'submitted';
-      }
-      else if (grade == null) {
-        grade = status.workflow_state === 'graded' ? 'submitted' : 'not submitted';
-      }
-      else if (status.assignment_id in assignments && 'points_possible' in assignments[status.assignment_id]) {
-        const pointsGrade = parseFloat(status.grade);
-        grade = 'satisfactory';
-        if (assignments[status.assignment_id].points_possible > 0 && pointsGrade < assignments[status.assignment_id].points_possible) {
-          grade = 'not yet satisfactory';
-        }
-      }
-
-      grades[status.assignment_id] = grade.toLowerCase().trim();
-    });
-  }
-
-  function createTable(group, grades) {
-    let table = '<table class="ic-Table ic-Table--hover-row"><thead><tr>';
-    table += '<th style="width: 70%;" scope="col">Assessment</th>';
-    table += '<th style="width: 30%;" scope="col">Grade</th>';
-    table += '</tr></thead>';
-    table += '<tbody>';
-
-    let isProgress = false;
-    let isIncompetent = false;
-
-    group.assignments.forEach((assignment) => {
-      console.log(assignment);
-      const grade = assignment.id in grades ? grades[assignment.id] : 'NA';
-      if (grade === 'submitted' || grade === 'not submitted' || grade === 'NA') isProgress = true;
-      else if (grade !== 'satisfactory' && grade !== 'excused') isIncompetent = true;
-      table += '<tr>';
-      table += `<td><a href="${assignment.url}">${assignment.name}</a></td>`;
-
-      let prettyGrade = '';
-      if (grade === 'satisfactory') {
-        prettyGrade = '<span style="color: #32b332;">Satisfactory</span>';
-      }
-      else if (grade === 'not yet satisfactory') {
-        prettyGrade = '<span style="color: #ee3a3d;">Not Yet Satisfactory</span>';
-      }
-      else if (grade === 'submitted') {
-        prettyGrade = '<span style="color: #2f74f4;">Submitted</span>';
-      }
-      else {
-        prettyGrade = `<span style="text-transform: capitalize;">${grade}</span>`;
-      }
-      table += `<td><p>${prettyGrade}</p></td>`;
-      table += '</tr>';
-    });
-
-    const result = isProgress ? 'In Progress' : (isIncompetent ? 'Not Yet Competent' : 'Competent');
-    table += '<tr style="font-size: 1.1rem;">';
-    //table += '<td>&nbsp;</td>';
-    table += '<td style="text-align: right;"><p><strong>Unit Result:&nbsp;</strong></p></td>';
-    table += `<td><p>${result}</p></td>`;
-    table += '</tr></tbody></table><p>&nbsp;</p>';
-
-    return table;
-  }
-
-  function createResultTable(result) {
-    let table = '<table className="ic-Table" style="width: 100%; border-top-color: #c7cdd1; border-top-style: double; font-size: 1.5rem;">';
-    table += '<tbody><tr>';
-    table += '<td style="width: 60%;">&nbsp;</td>';
-    table += '<td style="width: 20%; text-align: right;"><p><strong>Course Result:&nbsp;</strong></p></td>';
-    table += `<td style="width: 20%;"><p>${result}</p></td>`;
-    table += '</tr></tbody></table>';
-
-    return table;
+  function createLoadingTableBody(total, fails, passes) {
+    let content = `<tr><td style="padding: 0.5rem 0 0 0.5rem;">&nbsp;</td></tr>`;
+    content += `<tr><td style="padding: 0.5rem 0 0 0.5rem;  text-align: center;">Loading...</td></tr>`;
+    content += `<tr><td style="padding: 0.5rem 0 0 0.5rem;">&nbsp;</td></tr>`;
+    return content;
   }
 
   function nextURL(linkTxt) {
